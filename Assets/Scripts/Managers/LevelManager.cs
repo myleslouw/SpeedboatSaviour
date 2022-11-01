2c083a8f0d6510b3dde432e26f3676e0a3d8e743
myleslouw <56117169+myleslouw@users.noreply.github.com>
2022-11-01 14:21:32 +0200

    small scene changes

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] GameObject NPCSinLevel;
-    [SerializeField] Transform SpawnPoint;
-    
+    [SerializeField] GameObject[] NPCSinLevel;            //empty that holds the NPCs as children in their respective positions (Index is the level num)
+    [SerializeField] Transform[] SpawnPoints;              //the spawning point for the pollution to spawn around               (index is the level num)
+
+    [SerializeField] GameObject PrevArea;               //the area the player was prev in/just finished must now be enabled which shows the fish/sea life
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Water/MovementLerp.cs
+++ Assets/Scripts/Water/MovementLerp.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class MovementLerp : MonoBehaviour
@@ -11,11 +12,18 @@ public class MovementLerp : MonoBehaviour
 
     void Start()
     {
+        SyncItems();
     }
 
     void Update()
     {
         transform.position = new Vector3(-1.4f, Mathf.Sin(-Time.time * speed) * height + 1.39f, -2.2f);
         transform.Rotate(0, 0.1f, 0);
+    }
+
+    private async Task SyncItems()
+    {
+        //syncs the bottle nad the water
+        Task.Delay(200);
     }
 }
